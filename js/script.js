65a8bb80613d8d4763a8b67c5fb88fa297835c95
Axel Olsson <axelolsson@users.noreply.github.com>
2019-11-06 12:49:35 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Globala variabler
+
+const wordList;      // Array: med spelets alla ord
+let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+
+let guesses = 0;     // Number: håller antalet gissningar som gjorts
+let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+
+let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
+let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
+let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
+let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+
+// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+// Funktion som slumpar fram ett ord
+// Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
+// Funktion som körs när du trycker på bokstäverna och gissar bokstav
+// Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
+// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
