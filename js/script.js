528e2be788a5b9c666028be46331ccae9952e5ee
Andreas Ny <andreas.ny@chasacademy.se>
2019-11-08 13:32:04 +0100

    Filter more words from list Add placeholder to letterBoxes Progress on `checkLetterValue` function

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ let hangmanImg; // Sträng: sökväg till bild som kommer visas (och ändras) fe
 
 let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
 let startGameBtnEl = document.getElementById("startGameBtn"); // DOM-nod: knappen som du startar spelet med
-let letterButtonEls = document.querySelectorAll('#letterButtons button'); // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls = document.querySelector('#letterBoxes > ul'); // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterButtonEls = document.querySelectorAll("#letterButtons button"); // Array av DOM-noder: Knapparna för bokstäverna
+let letterBoxEls = document.querySelector("#letterBoxes > ul"); // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 // Funktion som slumpar fram ett ord
@@ -43,7 +43,12 @@ function removeWordsContainingDash(item) {
 */
 // const wordList = wordsFromTxt.filter(removeWordsContainingDash);
 
-// Save wordsFromText items, that does not include a '-' in wordList 
+// Save wordsFromText items, that does not include a '-' in wordList
+wordsFromTxt = wordsFromTxt.filter(item => !item.includes("á"));
+wordsFromTxt = wordsFromTxt.filter(item => !item.includes("é"));
+wordsFromTxt = wordsFromTxt.filter(item => !item.includes("ó"));
+wordsFromTxt = wordsFromTxt.filter(item => !item.includes("'"));
+wordsFromTxt = wordsFromTxt.filter(item => !item.includes("ü"));
 const wordList = wordsFromTxt.filter(item => !item.includes("-"));
 
 // Log wordList array
@@ -56,7 +61,7 @@ startGameBtnEl.addEventListener("click", startGame);
 function startGame() {
   generateRandomWord();
   createLetterBoxes();
-};
+}
 
 /*
 Skapa en funktion, kalla den för generateRandomWord().
@@ -70,7 +75,7 @@ function generateRandomWord() {
   // startGameBtnEl.disabled = true;
 
   return selectedWord;
-};
+}
 
 /*
 Baserat på längden i `selectedWord` (loopa/iterera):
@@ -79,27 +84,76 @@ Använd `.appendChild()` för att lägga till det skapade elementet inuti `lette
 */
 
 function createLetterBoxes() {
+  letterBoxEls.innerHTML = "";
 
-  letterBoxEls.innerHTML = '';
-
-  for (let i = 0; i < selectedWord.length; i++) {
+  for (let i = 0; i < selectedWord.length - 1; i++) {
     // console.log(selectedWord[i]);
-    let liEl = document.createElement('li');
-    let liElInput = document.createElement('input');
-    liElInput.setAttribute('type', 'text');
-    liElInput.setAttribute('value', '');
-    liElInput.setAttribute('disabled', '');
-    liEl.appendChild(liElInput);
-    letterBoxEls.appendChild(liEl);
+    let liEl = document.createElement("li");
+    let liElInput = document.createElement("input");
+    liElInput.setAttribute("type", "text");
+    liElInput.setAttribute("value", selectedWord[i]);
+    liElInput.setAttribute("placeholder", "__");
+    liElInput.setAttribute("disabled", "");
+    liEl.appendChild(liElInput); // make the input a child of the list-element
+    letterBoxEls.appendChild(liEl); // make the list and input a child of letterbox
   }
-};
+}
 
 // Listen to clicks on letters
 letterButtonEls.forEach(letter => {
+  letter.addEventListener("click", function() {
+    // console.log(selectedWord.split(''));
+    
+    checkLetterValue2(letter.value);
 
-  letter.addEventListener('click', function() {
-    console.log(letter.value);
   });
-  
 });
 
+function checkLetterValue(letterValue) {
+  // console.log(letterValue);
+  
+  switch (letterValue) {
+    case 'A':
+    case 'a':
+          console.log(selectedWord);
+      break;
+  
+    default:
+      console.log("Ej godkänd bokstav");
+      
+      break;
+  }
+}
+
+// callback
+// 6.1 check letter value
+/*
+let callback = function (letter) {
+
+  console.log(letter);
+  debugger;
+}
+*/
+
+function checkLetterValue2(letterValue) {
+
+  selectedWordArray = selectedWord.split(''); // split word into array
+  selectedWordArray.pop(); // remove last position, unwanted " "
+  console.log(selectedWord);
+  
+  // selectedWordArray.forEach(letter => {
+
+    if (selectedWordArray.includes(letterValue) && guesses < 6) {
+      console.log(selectedWordArray.find(letterValue));
+      
+      
+    } else if (!selectedWordArray.includes(letterValue) && guesses < 6){
+      guesses++;
+      console.log(guesses);
+    } else {
+      console.log("Game Över");
+      
+    }
+    
+  // });
+};
